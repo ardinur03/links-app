9c0bc5239aa6b247d5cbe7be554476d6ed18df44
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-07-29 20:26:49 +0700

    FIX : add boolean data type conditional isCrud for form component

--- app/Http/Livewire/Auth/FormRegister.php
+++ app/Http/Livewire/Auth/FormRegister.php
@@ -6,6 +6,13 @@ use Livewire\Component;
 
 class FormRegister extends Component
 {
+    public $isCrud = false;
+
+    public function mount($isCrud = false)
+    {
+        return $this->isCrud = $isCrud;
+    }
+
     public function render()
     {
         return view('livewire.auth.form-register');
