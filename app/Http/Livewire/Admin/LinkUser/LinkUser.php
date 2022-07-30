adcf2ed588832f76e447e1cc7516018045c76ab9
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-07-30 21:26:37 +0700

    FEAT : add View

---
+++ app/Http/Livewire/Admin/LinkUser/LinkUser.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Admin\LinkUser;
+
+use Livewire\Component;
+
+class LinkUser extends Component
+{
+    public function render()
+    {
+        return view('livewire.admin.link-user.link-user');
+    }
+}
---
+++ app/Http/Livewire/Admin/User/ShowUser.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Livewire\Admin\User;
+
+use Livewire\Component;
+
+class ShowUser extends Component
+{
+
+    public $users = [];
+
+    public function mount($users)
+    {
+        $this->users = $users;
+    }
+
+    public function render()
+    {
+        return view('livewire.admin.user.show-user');
+    }
+}
