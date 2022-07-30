06d11926d1fe27e197ee23ac8b6aba6e385628c7
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2022-07-30 21:23:47 +0700

    FIX : add model component in users crud

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -2,13 +2,19 @@
     <div class="row">
         <div class="col-12">
             <h2 class="content-title">Users</h2>
-            <h5 class="content-desc mb-4">All User Data</h5>
+            <h5 class="content-desc mb-3">All User Data</h5>
         </div>
     </div>
 
-    <div class="row card shadow-md">
+    <div class="row mb-3">
         <div class="col-12">
-            <table class="table">
+            <a href="{{ route('users.create') }}" class="btn btn-sm btn-success"><i class="bi bi-person-plus-fill"></i></a>
+        </div>
+    </div>
+
+    <div class="row card shadow-md p-3">
+        <div class="col-12">
+            <table class="table table-sm table-responsive">
                 <thead>
                     <tr>
                         <th scope="col">#</th>
@@ -27,8 +33,6 @@
                             <td>{{ date('d M Y', strtotime($user->created_at)) }}</td>
                             <td class="text-center">
                                 <div class="btn-group" role="group" aria-label="Basic example">
-                                    <a href="{{ route('users.create') }}" class="btn btn-sm btn-success"><i
-                                            class="bi bi-plus-lg"></i></a>
                                     <a href="" class="btn btn-sm btn-secondary"><i class="bi bi-eye"></i></a>
                                     <a href="" class="btn btn-sm btn-info"><i class="bi bi-pencil-fill"></i></a>
                                     <a href="" class="btn btn-sm btn-danger"><i class="bi bi-trash"></i></a>
@@ -44,5 +48,8 @@
                 </tbody>
             </table>
         </div>
+
+        <livewire:admin.user.show-user :users="$user->id" />
+
     </div>
 </x-app-layout>
